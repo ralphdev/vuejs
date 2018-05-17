9d06df5fd73f504d0f4acbee1c68b8e5ffcbf5d4
ralphdev <rafa.developers@gmail.com>
2018-05-17 10:26:32 -0500

    Metodo Editar y Eliminar

--- componentes/js/app.js
+++ componentes/js/app.js
@@ -4,15 +4,16 @@ Vue.component('tasks', {
 	<section class="todoapp">
 		<header class="header">
 			<h1>Tareas</h1>
-			<input type="text" class="new-todo" v-model="newTask" v-on:keyup.enter="add">
+			<input type="text" class="new-todo" v-model="newTask" v-on:keyup.enter="add" placeholder="Agrega Tareas">
 		</header>
 		<section>
 			<ul class="todo-list">
 				<li class="todo" is="task" v-for="task in tasks" :task="task"></li>
 			</ul>
 		</section>
-		<footer class="footer">
-			<span class="todo-count">Completadas {{ completed }}</span> | <span class="todo-count">Completadas {{ incompleted }}</span>
+		<footer class="footer" v-show="tasks.length">
+			<span class="todo-count">Completadas {{ completed }}</span>
+			<span class="todo-count">Completadas {{ incompleted }}</span>
 		</footer>
 	</section>`,
 	data: function(){
@@ -58,15 +59,44 @@ Vue.component('task', {
 	<li :class="Classess">
 		<div class="view">
 			<input type="checkbox" class="toggle" v-model="task.completed" />
-			<label v-text="task.title"></label>
+			<label v-text="task.title" @dblclick="edit()"></label>
+			<button class="destroy" @click="remove()"></button>
 		</div>
-		
+		<input type="text" class="edit" v-model="task.title" @keyup.enter="doneEdit()" @blur="doneEdit()" @keyup.esc="cancelEdit()"/>
 	</li>
 	`,
+	data: function(){
+		return {
+			editing: false,
+			cacheBeforeEdit: ''
+		}
+	},
+	methods: {
+		edit: function(){
+			this.cacheBeforeEdit = this.task.title;
+			this.editing = true;
+		},
+		doneEdit: function(){
+			if(! this.task.title){
+				this.remove();
+			}
+			this.editing = false;
+			this.task.title = this.cacheBeforeEdit;
+		},
+		cancelEdit: function(){
+			this.editing = false,
+			this.task.title = this.cacheBeforeEdit;
+		},
+		remove: function(){
+			//hay que llamar al componente padre
+			var tasks = this.$parent.tasks;
+			tasks.splice(tasks.indexOf(this.task), 1);
+		}
+	},
 	computed:{
 		Classess: function(){
 			//return ['glyphicon', this.task.completed ? 'glyphicon-check': 'glyphicon-unchecked']
-			return { completed: this.task.completed };
+			return { completed: this.task.completed, editing: this.editing };
 		}
 	}
 
